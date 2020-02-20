5cfee75c0ddee7728d633f3cfc332de92c2a5213
Alexei <mail@mail.by>
2020-02-20 16:03:17 +0300

    add book layout rendering

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable prefer-template */
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const handlebars = require('express-handlebars');
 
 const { mongoose } = require('./db/mongoose'); // connecteed to db
-
 const saveBookDoc = require('./book-parser');
 const saveVideoDoc = require('./video-parser');
 
+const Book = require('./models/book');
+
+handlebars.create({ defaultLayout: 'main' });
 const app = express();
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+const viewsPath = path.resolve(path.resolve(__dirname, '../views'));
+app.set("views", viewsPath);
 
 app.use(bodyParser.json());
 
@@ -23,7 +33,6 @@ app.post('/add', async (req, res) => {
   let savedVideo;
 
   const pagePath = req.headers['x-orig-name'];
-  // eslint-disable-next-line prefer-destructuring
   data.pageName = pagePath.slice(pagePath.lastIndexOf('\\') + 1).split('.json')[0];
   if (data.category.indexOf('Books') !== -1) {
     savedBook = await saveBookDoc(data);
@@ -39,6 +48,36 @@ app.post('/add', async (req, res) => {
   } else {
     res.status(400).end();
   }
+});
+
+app.get('/2',(req, res) => {
+  Book.find({}, (err, books) => {
+    if (err) throw err;
+    console.log(books[0]);
+    res.render('book', books[0]);
+  }).limit(1);
+});
+
+// // Обобщенный обработчик 404 (промежуточное ПО)
+// app.use((req, res, next) => {
+//   res.status(404);
+//   res.render('404');
+// });
+
+// // Обработчик ошибки 500 (промежуточное ПО)
+// app.use( (err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500);
+//   res.render('500');
+// });
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.use('/', (req, res, next) => {
+  res.redirect(303, '/');
 });
 
 app.listen(3000, () => {
